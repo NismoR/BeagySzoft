97efea792bfa36946956cc8d41dbcf1a3430a474
Bence Acs <acs.bence94@gmail.com>
2018-04-19 22:32:16 +0200

    AB: Changed GUI from JFrame to JPanel, basic drawing

--- code/Heroes/src/heroes/GUI.java
+++ code/Heroes/src/heroes/GUI.java
@@ -4,28 +4,38 @@
  */
 package heroes;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 /**
  *
  * @author ABence
  */
-class GUI extends JFrame {
+public class GUI extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private Control ctrl;
+	private final int WINDOW_WIDTH = 950, WINDOW_HEIGHT = 650;
+	private final int FIELD_WIDTH = 70, FIELD_HEIGHT = 70;
+	private final int TABLE_OFFSET_X = 50, TABLE_OFFSET_Y = 50;
+	private final int TABLE_SIZE_X = 8, TABLE_SIZE_Y = 8;
+	private JFrame j;
 
 	GUI(Control c) {
-		super("Heroes");
+		j = new JFrame("Heroes");
 		ctrl = c;
-		setSize(500, 350);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(null);
+		j.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		j.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		j.setLayout(null);
 
 		JMenuBar menuBar = new JMenuBar();
 
@@ -49,9 +59,24 @@ class GUI extends JFrame {
 		menuItem.addActionListener(new MenuListener());
 		menuBar.add(menuItem);
 
-		setJMenuBar(menuBar);
+		j.setJMenuBar(menuBar);
+		j.setContentPane(this);
+		j.setVisible(true);
+		repaint();
+	}
 
-		setVisible(true);
+	public JFrame getJ() {
+		return j;
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.lightGray);
+		g.fillRect(0, 0, 950, 650);
+		g.setColor(Color.black);
+		g.setFont(new Font("Times New Roman", Font.BOLD, 24));
+		g.setColor(Color.blue);
+		g.fillRect(50, 50, 560, 510);
 	}
 
 	private class MenuListener implements ActionListener {
