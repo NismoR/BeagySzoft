72a6efb04295be1f09d6952cb95d93890d8aaf71
Bence Acs <acs.bence94@gmail.com>
2018-04-22 21:52:52 +0200

    AB: GameTurn is now an enum

--- code/Heroes/src/heroes/GameState.java
+++ code/Heroes/src/heroes/GameState.java
@@ -3,19 +3,23 @@ package heroes;
 import java.io.Serializable;
 
 public class GameState implements Serializable{
-	/**
-	 * 
-	 */
+
+	public enum GameTurn {
+		NOT_STARTED,
+		PLAYER_CLIENT,
+		PLAYER_SERVER
+	}
+	
 	private static final long serialVersionUID = 1L;
 	
 	private int board_size = 8;
 	public int time;
-	public int turn;
+	public GameTurn turn;
 	public int[][] board_bg;
 	
 	public GameState(){
 		time = 0;
-		turn = 0;
+		turn = GameTurn.NOT_STARTED;
 		board_bg = new int[board_size][board_size];
 		for(int i = 0; i < board_size; i++){
 			for(int j = 0; j < board_size; j++){
