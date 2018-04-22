7ac8a57858137339dc5f09f9cea1a1a4ee8278cd
Bence Acs <acs.bence94@gmail.com>
2018-04-22 21:59:04 +0200

    AB: FieldType is enum, too, added boolean for steppable field

--- code/Heroes/src/heroes/GameState.java
+++ code/Heroes/src/heroes/GameState.java
@@ -6,8 +6,16 @@ public class GameState implements Serializable{
 
 	public enum GameTurn {
 		NOT_STARTED,
+		INITING_MAP,
 		PLAYER_CLIENT,
 		PLAYER_SERVER
+	}
+	
+	public enum FieldType {
+		NOT_AVAILABLE,
+		FREE,
+		ATTACKABLE,
+		CURRENT
 	}
 	
 	private static final long serialVersionUID = 1L;
@@ -15,15 +23,18 @@ public class GameState implements Serializable{
 	private int board_size = 8;
 	public int time;
 	public GameTurn turn;
-	public int[][] board_bg;
+	public boolean[][] steppable;
+	public FieldType[][] board_bg;
 	
 	public GameState(){
 		time = 0;
 		turn = GameTurn.NOT_STARTED;
-		board_bg = new int[board_size][board_size];
+		board_bg = new FieldType[board_size][board_size];
+		steppable = new boolean[board_size][board_size];
 		for(int i = 0; i < board_size; i++){
 			for(int j = 0; j < board_size; j++){
-				board_bg[i][j] = 0;
+				board_bg[i][j] = FieldType.NOT_AVAILABLE;
+				steppable[i][j] = false;
 			}
 		}
 	}
@@ -34,6 +45,7 @@ public class GameState implements Serializable{
 		for(int i = 0; i < board_size; i++){
 			for(int j = 0; j < board_size; j++){
 				board_bg[i][j] = gs.board_bg[i][j];
+				steppable[i][j] = gs.steppable[i][j];
 			}
 		}
 	}
