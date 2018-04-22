8d4ab22ee00c17aff4284a1248016acf7ec6e06a
Bence Acs <acs.bence94@gmail.com>
2018-04-22 19:48:54 +0200

    AB: Added basic Hero class

---
+++ code/Heroes/src/heroes/Hero.java
@@ -0,0 +1,19 @@
+package heroes;
+
+public abstract class Hero {
+	private HeroType type;
+	private int player_id;
+	
+	public Hero(HeroType type, int player_id){
+		this.type = type;
+		this.player_id = player_id;
+	}
+	
+	public HeroType get_type(){
+		return this.type;
+	}
+	
+	public int get_player_id(){
+		return this.player_id;
+	}
+}
---
+++ code/Heroes/src/heroes/HeroType.java
@@ -0,0 +1,7 @@
+package heroes;
+
+public enum HeroType {
+	WARRIOR,
+	ARCHER,
+	MAGE
+}
