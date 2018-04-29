7fdb716951b6138f06d219ac27bf7978556a800f
Bence Acs <acs.bence94@gmail.com>
2018-04-29 10:15:05 +0200

    AB: Added basic equipment handling to heros

--- code/Heroes/src/heroes/Hero.java
+++ code/Heroes/src/heroes/Hero.java
@@ -1,8 +1,14 @@
 package heroes;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import heroes.equipments.Equipment;
 
 public abstract class Hero {
+	
+	private int MAX_EQUIPMENT_NR=6;
 	
 	public enum HeroType {
 		WARRIOR,
@@ -17,10 +23,12 @@ public abstract class Hero {
 	
 	private HeroType type;
 	private PlayerID player_id;
+	private List<Equipment> equips;
 	
 	private int x=-1,y=-1;
 	
 	public Hero(HeroType type, PlayerID player_id){
+		equips = new ArrayList<Equipment>();
 		this.type = type;
 		this.player_id = player_id;
 	}
@@ -39,6 +47,29 @@ public abstract class Hero {
 	
 	public int get_y(){
 		return y;
+	}
+	
+	public int get_max_eq_nr(){
+		return MAX_EQUIPMENT_NR;
+	}
+	
+	public boolean add_equip(Equipment e){
+		if(equips.size()>=MAX_EQUIPMENT_NR){
+			return false;
+		}
+		equips.add(e);
+		return true;
+	}
+	
+	public Equipment get_equip(int index){
+		if(index<0 || index>=equips.size()){
+			return null;
+		}
+		return equips.get(index);
+	}
+	
+	public List<Equipment> get_equips(){
+		return equips;
 	}
 	
 	public void set_coordinates(int x, int y){
