b42d0f56cbce4264f4f570f9e52b5372c54459ba
Bence Acs <acs.bence94@gmail.com>
2018-04-29 09:59:50 +0200

    AB: Added basics of equipments

---
+++ code/Heroes/src/heroes/equipments/Equipment.java
@@ -0,0 +1,37 @@
+package heroes.equipments;
+
+import heroes.Hero.HeroType;
+
+public abstract class Equipment {
+	public enum EqType {
+		WOODEN_SWORD,
+		WOODEN_SHIELD
+	}
+	private EqType eq_type;
+	private String eq_name;
+	private int value;
+	private HeroType available_for;
+	
+	public Equipment(EqType eq_type,String eq_name,int value,HeroType available_for){
+		this.eq_type = eq_type;
+		this.eq_name = eq_name;
+		this.value = value;
+		this.available_for = available_for;
+	}
+	
+	public EqType get_type() {
+		return eq_type;
+	}
+	
+	public String get_name(){
+		return eq_name;
+	}
+	
+	public int get_value(){
+		return value;
+	}
+	
+	public HeroType get_available_for(){
+		return available_for;
+	}
+}
---
+++ code/Heroes/src/heroes/equipments/WoodenSword.java
@@ -0,0 +1,10 @@
+package heroes.equipments;
+
+public class WoodenSword extends Equipment{
+
+	public WoodenSword() {
+		super(EqType.WOODEN_SWORD, "WoodenSword", 1, null);
+		// TODO Auto-generated constructor stub
+	}
+
+}
